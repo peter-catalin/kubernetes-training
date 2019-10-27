6297ac7cf6100741d08ae653e9652a99a876f59a
Peter <peter.catalin@lendismart.com>
2019-10-27 22:10:01 +0100

    Adding echo statements to the deploy file

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,24 @@
 # the GIT SHA of the latest commit. This allows us to change the image used by our containers in the last part of the file. If we were to
 # set the latest image with the kubectl set image that would not work because internally Kubernetes thinks it is already running the lastest image.
 # By using a unique identifier for our image we gracefully solve that problem.
+echo About to build and tag images ...
 docker build -t petercatalin/docker-training-multi-client:latest -t petercatalin/docker-training-multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t petercatalin/docker-training-multi-server:latest -t petercatalin/docker-training-multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t petercatalin/docker-training-multi-worker -t petercatalin/docker-training-multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t petercatalin/docker-training-multi-worker:latest -t petercatalin/docker-training-multi-worker:$SHA -f ./worker/Dockerfile ./worker
+echo Images have been created and tagged
 
 # Once the images are successfully generated we need to push them to DockerHub.
+echo Pushing images as latest ...
 docker push petercatalin/docker-training-multi-client:latest
 docker push petercatalin/docker-training-multi-server:latest
 docker push petercatalin/docker-training-multi-worker:latest
+echo Images have been pushed
+
+echo Pushing images with SHA as tag ...
 docker push petercatalin/docker-training-multi-client:$SHA
 docker push petercatalin/docker-training-multi-server:$SHA
 docker push petercatalin/docker-training-multi-worker:$SHA
+echo Images have been pushed
 
 # After the images are pushed we ask Travis to apply all our configuration files located in the k8s directory
 # in order to update our cluster as needed.
